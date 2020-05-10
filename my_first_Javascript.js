fa6d05af5e5910e20683afb3a516806c5e723a93
marcellotyello321 <60796700+marcellotyello321@users.noreply.github.com>
2020-05-09 23:43:08 -0300

    Create my_first_Javascript.js

---
+++ my_first_Javascript.js
@@ -0,0 +1,36 @@
+# 2001-project
+Basic codes
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+	<title>My First Javascript</title>
+ </head>
+<body>
+	<p>My First Javascript</p>
+<script type="text/javascript">
+	
+  alert('Ola mundo Javascript!')
+  
+  // declaraçâo das variaveis//
+  var nota1 = 45;
+  var nota2 = 3;
+  var nota3 = 87;
+  var nota4 = 10;
+  // declaraçâo de duas variaveis e processamento//
+  var soma = (nota1 + nota2 + nota3 + nota4);
+  var resultado = (soma/4);
+
+  //para "concatenar" texto com variável, use o + 
+  
+
+  // saida de dados //
+  document.write("A nota de Marcello é");
+  document.write(resultado);
+  // para "concatenar" texto com variavel, use o + //
+
+
+  </script>
+ </body>
+</html>
