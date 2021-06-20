a7f366cfebeae0d19e0abb55ae7ad730ff78543a
marcellotyello321 <60796700+marcellotyello321@users.noreply.github.com>
2021-06-20 12:38:31 -0300

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,31 @@
+let day;
+switch (new Date().getDay()){
+    case 0:
+        day = "Sunday";
+        break;
+    
+    case 1:
+        day = "Monday";
+        break;
+    
+    case 2:
+        day = "Tuesday";
+        break;
+    
+    case 3:
+        day = "Wednesday";
+        break;
+    
+    case 4:
+        day = "Thrusday";
+        break;
+    
+    case 5:
+        day = "Friday";
+        break;
+    
+    case 6: 
+        day = "Saturday";
+
+}
+document.getElementById('day').innerHTML = "Today Is " + day;
