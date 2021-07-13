e96f4b2452d224f673e64c2c1c6a6bdb8f3da8a3
marcellotyello321 <60796700+marcellotyello321@users.noreply.github.com>
2021-07-13 19:19:35 -0300

    Create ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,19 @@
+let passagem = 10.70;
+
+
+if ( passagem > 11.20){
+    intermunicipal = 'Santana De Parnaiba'
+    console.log(intermunicipal);
+
+}else if (passagem > 12.00 && passagem < 15.00 ){
+    intermunicipal = 'Lapa'
+    console.log(intermunicipal);
+
+}else if (passagem > 8.30 && passagem <8.00){
+    intermunicipal = 'Osasco'
+    console.log(intermunicipal)
+
+}else {
+    intermunicipal = 'garagem'
+    console.log(intermunicipal);
+}
